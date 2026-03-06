0a55f9492b9f266aae7e46cdd476941ab90c6bc3
Thomas Nguyen <thomasnguyen15462@gmail.com>
2026-03-05 23:29:24 -0500

    moved modal to the top

--- src/pages/MyProjects/components/ProjectCard/ProjectCard.jsx
+++ src/pages/MyProjects/components/ProjectCard/ProjectCard.jsx
@@ -13,42 +13,7 @@ function ProjectCard({ project, onClick }) {
 
 
   return (
-    <Col id={styles["project-card"]} className="rounded-4 p-4" onClick={onClick} role="button">
-      <div className="d-flex justify-content-between">
-        <CheckSquare size={50} color={project.color} />
-        {project.owner === 1 && (
-          <X size={25} color="#EF4444"
-          onClick = {(e) => {
-            e.stopPropagation();
-            handleShow();
-          }}
-          />
-        )}
-      </div>
-
-      <div className="mb-5 mt-3" style={{ height: 70 }}>
-        <h5 className="fw-bold mb-3">{project.name}</h5>
-        <p className="text-muted small">
-          {project.description.length > 100
-            ? project.description.slice(0, 100) + "..."
-            : project.description}
-        </p>
-      </div>
-
-      <div className="d-flex justify-content-between">
-        <span className="text-muted small">Progress</span>
-        <span className="text-muted small">{progress}%</span>
-      </div>
-      <ProgressBar
-        now={progress}
-        className="mb-1"
-        style={{ height: 8, "--bs-progress-bar-bg": project.color }} />
-      <p className="text-muted small">
-        {project.completedTasks} of {project.totalTasks} tasks completed
-      </p>
-
-      <hr />
-
+    <>
       <Modal id = {styles["delete-warning"]} show={show} onHide={handleClose}>
         <Modal.Header className = "gap-2 border-0">
           <Modal.Title id = {styles["del-title"]}>Delete Project?</Modal.Title>
@@ -69,17 +34,51 @@ function ProjectCard({ project, onClick }) {
           </Button>
         </Modal.Footer>
       </Modal>
+      <Col id={styles["project-card"]} className="rounded-4 p-4" onClick={onClick} role="button">
+        <div className="d-flex justify-content-between">
+          <CheckSquare size={50} color={project.color} />
+          {project.owner === 1 && (
+            <X size={25} color="#EF4444"
+            onClick = {(e) => {
+              e.stopPropagation();
+              handleShow();
+            }}
+            />
+          )}
+        </div>
 
-      <div className="d-flex justify-content-between text-muted small">
-        <span><Users size={16} className="me-1" />{project.numMembers}</span>
-        <span>
-          <Calendar size={16} className="me-1" />
-          {new Date(project.dueDate).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric" })}
-        </span>
-      </div>
-    </Col>
+        <div className="mb-5 mt-3" style={{ height: 70 }}>
+          <h5 className="fw-bold mb-3">{project.name}</h5>
+          <p className="text-muted small">
+            {project.description.length > 100
+              ? project.description.slice(0, 100) + "..."
+              : project.description}
+          </p>
+        </div>
 
-    
+        <div className="d-flex justify-content-between">
+          <span className="text-muted small">Progress</span>
+          <span className="text-muted small">{progress}%</span>
+        </div>
+        <ProgressBar
+          now={progress}
+          className="mb-1"
+          style={{ height: 8, "--bs-progress-bar-bg": project.color }} />
+        <p className="text-muted small">
+          {project.completedTasks} of {project.totalTasks} tasks completed
+        </p>
+
+        <hr />
+
+        <div className="d-flex justify-content-between text-muted small">
+          <span><Users size={16} className="me-1" />{project.numMembers}</span>
+          <span>
+            <Calendar size={16} className="me-1" />
+            {new Date(project.dueDate).toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric" })}
+          </span>
+        </div>
+      </Col>
+    </>
   );
 }
 
